eee4f8fe1c977567cac560ce41e285e48e6f29e9
jeremyfelt <jeremy.felt@gmail.com>
2015-03-09 17:03:48 -0700

    Continue configuring navigation menus for home page

--- parts/before-main.php
+++ parts/before-main.php
@@ -1,18 +1,28 @@
 <?php
 
-$mega_menu_args = array(
-	'theme_location'  => 'mega-menu',
-	'menu'            => 'mega-menu',
-	'container'       => 'div',
-	'container_class' => false,
-	'container_id'    => 'mega-menu',
-	'menu_class'      => null,
-	'menu_id'         => null,
-	'items_wrap'      => '<ul>%3$s</ul>',
-	'depth'           => 5,
-);
+/*
+ * On the home page, we display a series of navigation menus at the top.
+ */
+if ( is_front_page() ) :
 
+	$mega_menu_args = array(
+		'theme_location'  => 'mega-menu',
+		'menu'            => 'mega-menu',
+		'container'       => 'div',
+		'container_class' => false,
+		'container_id'    => 'mega-menu',
+		'menu_class'      => null,
+		'menu_id'         => null,
+		'items_wrap'      => '<ul>%3$s</ul>',
+		'depth'           => 5,
+	);
 ?>
-<section class="single row">
-	<div class="column one"><?php wp_nav_menu( $mega_menu_args ); ?></div>
+<section class="single row mega-menu">
+	<div class="column one">
+		<?php wp_nav_menu( $mega_menu_args ); ?>
+
+		<div class="top-level-links-label">WSU Campuses</div>
+		<div class="search-label">Search</div>
+	</div>
 </section>
+<?php endif;
