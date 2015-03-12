002f73d378deeb8112f6c2c77bdecdef5b652242
jeremyfelt <jeremy.felt@gmail.com>
2015-03-12 10:49:30 -0700

    Refactor HTML markup for the logo, mega, and signature menu area

--- parts/before-main.php
+++ parts/before-main.php
@@ -29,16 +29,29 @@ if ( is_front_page() ) :
 		'depth'           => 1,
 	);
 ?>
-<header class="triptych row wsu-home-navigation">
-	<div class="column one">
-		<div class="wsu-logo">WSU LOGO</div>
-	</div>
-	<div class="column two wsu-mega-nav-placeholder">
-		<?php wp_nav_menu( $mega_menu_args ); ?>
-	</div>
-	<div class="column three wsu-other-nav-placeholder">
-		<div class="top-level-links-label">WSU Campuses</div>
-		<div class="search-label">Search</div>
-	</div>
+<header class="main-header wsu-home-navigation">
+	<section class="single triptych row">
+		<div class="column one">
+			<div class="wsu-logo">WSU LOGO</div>
+		</div>
+		<div class="column two">
+			<!-- Empty with purpose. -->
+		</div>
+		<div class="column three wsu-other-nav-placeholder">
+			<div class="top-level-links-label">WSU Campuses</div>
+			<div class="search-label">Search</div>
+		</div>
+	</section>
+	<section class="single triptych row">
+		<div class="column one wsu-signature-nav-container">
+			<?php wp_nav_menu( $signature_menu_args ); ?>
+		</div>
+		<div class="column two wsu-mega-nav-container">
+			<?php wp_nav_menu( $mega_menu_args ); ?>
+		</div>
+		<div class="column three">
+			<!-- Empty with purpose. -->
+		</div>
+	</section>
 </header>
 <?php endif;
