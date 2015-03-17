8fd6f17b147a21c4a1744c5a99de4394503f57b6
jeremyfelt <jeremy.felt@gmail.com>
2015-03-17 13:16:17 -0700

    Adjust scripting to toggle mega menu on ghost menu click

--- js/wsu-home-navigation-view.js
+++ js/wsu-home-navigation-view.js
@@ -9,23 +9,21 @@ var wsuNavigation = wsuNavigation || {};
 
 		// Setup the events used in the overall application view.
 		events: {
-			'click #mega-menu ul li a': 'toggleNav',
+			'click #mega-menu-labels  ul li a': 'toggleNav',
 			'click .search-label': 'toggleSearch',
 			'click .top-level-links-label': 'toggleCampuses',
 			'click .campus-links-close': 'toggleCampuses'
 		},
 
 		toggleNav: function(evt){
-			if ( 2 === $(evt.currentTarget).parent().children().length ) {
-				evt.preventDefault();
-				var $nav_wrapper = $('.header-drawer-wrapper');
+			evt.preventDefault();
+			var $nav_wrapper = $('.header-drawer-wrapper');
 
-				if ( $nav_wrapper.hasClass('header-drawer-wrapper-hide') ) {
-					$nav_wrapper.parent('li').find('.sub-menu li a').css('color','#fff');
-					$nav_wrapper.removeClass('header-drawer-wrapper-hide');
-				} else {
-					$nav_wrapper.addClass('header-drawer-wrapper-hide');
-				}
+			if ( $nav_wrapper.hasClass('header-drawer-wrapper-hide') ) {
+				$nav_wrapper.parent('li').find('.sub-menu li a').css('color','#fff');
+				$nav_wrapper.removeClass('header-drawer-wrapper-hide');
+			} else {
+				$nav_wrapper.addClass('header-drawer-wrapper-hide');
 			}
 		},
 
