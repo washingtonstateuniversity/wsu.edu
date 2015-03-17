86daf94b4c6b61b9f41d02a9dcea9ac48de01b39
jeremyfelt <jeremy.felt@gmail.com>
2015-03-17 12:58:20 -0700

    Remove temporary WSU125 mark from footer area

--- parts/after-main.php
+++ parts/after-main.php
@@ -17,11 +17,6 @@ if ( is_front_page() ) :
 		'depth'           => 3,
 	);
 ?>
-	<section class="single row wsu-125-mark">
-		<div class="column one">
-			<img src="<?php echo get_stylesheet_directory_uri() . '/images/wsu-125-footer-temporary.png'; ?>" />
-		</div>
-	</section>
 	<section class="single row fat-footer-menu">
 		<div class="column one">
 			<?php wp_nav_menu( $wsu_fat_footer_args ); ?>
