903e8ab83e133075d1a950fae379f687a1fadf0d
jeremyfelt <jeremy.felt@gmail.com>
2015-03-18 16:57:24 -0700

    Start swapping feature slides on home page

--- js/script.js
+++ js/script.js
@@ -8,16 +8,34 @@ try{Typekit.load();}catch(e){}
 
 		$home_headlines.each( function() {
 			var $this = $(this),
+				id = $this.data('id'),
 				url = $this.data('anchor'),
 				headline = $this.data('headline'),
 				date = $this.data('date');
 
-			html += '<li><span class="home-headline-nav-headline"><a href="' + url + '">' + headline + '</a></span>' +
+			html += '<li data-id="' + id + '"><span class="home-headline-nav-headline"><a href="' + url + '">' + headline + '</a></span>' +
 					'<span class="home-headline-nav-date">' + date + '</span></li>';
 		});
 
 		html += '<li><span class="home-headline-nav-date"><a href="/features">...</a></span></li></ul>';
 		$features_container.append( html );
+
+		$('.home-headline-nav').on('click', function(evt) {
+			evt.preventDefault();
+
+			var id = $(evt.target).parent('li').data('id');
+
+			if ( undefined === id ) {
+				return;
+			}
+
+			if ( $('.features-container').hasClass('features-start') ) {
+				$('.features-container').removeClass('features-start');
+			}
+			$('.wsu-home-headline-wrapper-open').removeClass('wsu-home-headline-wrapper-open');
+			$('#' + id).addClass('wsu-home-headline-wrapper-open');
+		});
+
 	};
 
 	$(document).ready(
