c02557ba3d119ba701b2960e32f15c9f05ddce3d
jeremyfelt <jeremy.felt@gmail.com>
2015-03-20 14:03:16 -0700

    Stop fighting to reinvent the navigational wheel and use jQuery
    Rather than calculating a bunch of things to try and figure out
    how they should hide and size and rehide and resize—we can take
    existing jQuery solutions and apply them with ease.

--- js/wsu-home-navigation-view.js
+++ js/wsu-home-navigation-view.js
@@ -20,11 +20,11 @@ var wsuNavigation = wsuNavigation || {};
 			var $nav_wrapper = $('.header-drawer-wrapper');
 
 			if ( $nav_wrapper.hasClass('header-drawer-wrapper-open') ) {
+				$nav_wrapper.slideUp(400);
 				$nav_wrapper.removeClass('header-drawer-wrapper-open');
-				$nav_wrapper.css('margin-top', '-' + ( $nav_wrapper.height() + 5 ) + 'px');
 			} else {
+				$nav_wrapper.slideDown(400);
 				$nav_wrapper.addClass('header-drawer-wrapper-open');
-				$nav_wrapper.css('margin-top', '0' );
 			}
 		},
 
