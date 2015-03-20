9018d4f8d69eed5dadb0e1c1a349a8ef1fb110a1
jeremyfelt <jeremy.felt@gmail.com>
2015-03-20 11:08:45 -0700

    Enqueue the home script on all pages for now
    This unbreaks Proxima Nova temporarily.

--- functions.php
+++ functions.php
@@ -87,10 +87,7 @@ class WSU_Home_Theme {
 	 */
 	public function enqueue_scripts() {
 		wp_enqueue_script( 'wsu-home-typekit', 'https://use.typekit.net/roi0hte.js', array(), false, false );
-
-		if ( $this->is_wsu_site( 'wsu-home' ) && is_front_page() ) {
-			wp_enqueue_script( 'wsu-home', get_stylesheet_directory_uri() . '/js/wsu-home.min.js', array( 'backbone', 'wsu-home-typekit' ), $this->script_version(), true );
-		}
+		wp_enqueue_script( 'wsu-home', get_stylesheet_directory_uri() . '/js/wsu-home.min.js', array( 'backbone', 'wsu-home-typekit' ), $this->script_version(), true );
 	}
 
 	public function temp_enqueue_style() {
