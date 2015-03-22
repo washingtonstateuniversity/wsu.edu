e4abaa8b52be62dc67d52694e81f25ceb123cdcb
jeremyfelt <jeremy.felt@gmail.com>
2015-03-21 19:48:28 -0700

    Document existing WSU Feature Javascript, refactor positioning

--- js/wsu-feature-primary.js
+++ js/wsu-feature-primary.js
@@ -1,12 +1,53 @@
 (function($, window){
+	/**
+	 * Play a given video and clean up once it has played.
+	 *
+	 * @param evt
+	 */
 	play_video = function(evt) {
 		evt.target.play();
 		evt.target.addEventListener('ended', function() { this.load(); });
 		$('video').unbind('hover');
 	};
 
+	/**
+	 * Look for an exit video in the document and bind necessary events to
+	 * it when found.
+	 */
+	setup_exit_video = function() {
+		$video = $('.exit-video').find('video');
+		$video.on('hover', play_video);
+		$video.on('click', play_video);
+	};
+
+	/**
+	 * Create a script element to load in the YouTube iFrame API and insert it
+	 * into the document.
+	 */
+	load_youtube = function() {
+		var tag = document.createElement('script');
+
+		tag.src = "https://www.youtube.com/iframe_api";
+		var firstScriptTag = document.getElementsByTagName('script')[0];
+		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+	};
+
+	/**
+	 * Hold the YouTube Player objects created during the page load for
+	 * playback.
+	 *
+	 * @type {Array}
+	 */
 	var players = [];
 
+	/**
+	 * Callback function expected by the YouTube Iframe API. Without a function
+	 * with this name available in the global space, our use of the YouTube API
+	 * does not work.
+	 *
+	 * Loop through each of the inline YouTube Videos, gather the video information,
+	 * and set up objects representing the videos.
+	 */
 	window.onYouTubeIframeAPIReady = function() {
 		$('.inline-youtube-video').each(function(){
 			var video_id = $(this).data('video-id'),
@@ -31,22 +72,26 @@
 		});
 	};
 
+	/**
+	 * Callback function expected by the YouTube iFrame API based on our specification
+	 * in the events data above. We use this to attach a click event to any text with
+	 * a class of `start-{video_id}`. This allows initial interaction with the video to
+	 * begin inside the document.
+	 *
+	 * @param event
+	 */
 	window.onPlayerReady = function(event) {
 		$('.start-' + event.target.h.id).on('click', function() {
 			event.target.playVideo();
 		})
 	};
 
+	/**
+	 * Fire any actions that we need to happen once the document is ready.
+	 */
 	$(document).ready(function() {
-		$video = $('.exit-video').find('video');
-		$video.on('hover', play_video);
-		$video.on('click', play_video);
-
-		var tag = document.createElement('script');
-
-		tag.src = "https://www.youtube.com/iframe_api";
-		var firstScriptTag = document.getElementsByTagName('script')[0];
-		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+		load_youtube();
+		setup_exit_video();
 	});
 
 })(jQuery, window);
