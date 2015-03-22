45fed7b971e499fe20b82503013a7ee176f1aa22
jeremyfelt <jeremy.felt@gmail.com>
2015-03-21 19:36:53 -0700

    Reposition YouTube API scripting out of the document ready block

--- js/wsu-feature-primary.js
+++ js/wsu-feature-primary.js
@@ -5,49 +5,48 @@
 		$('video').unbind('hover');
 	};
 
+	var players = [];
+
+	window.onYouTubeIframeAPIReady = function() {
+		$('.inline-youtube-video').each(function(){
+			var video_id = $(this).data('video-id'),
+				video_height = $(this).data('video-height'),
+				video_width = $(this).data('video-width');
+
+			players.push(video_id);
+			players[video_id] = new YT.Player( 'youtube-video-' + video_id, {
+				height: video_height,
+				width: video_width,
+				videoId: video_id,
+				playerVars: {
+					modestbranding: 1,
+					showinfo: 0,
+					controls: 0,
+					rel: 0
+				},
+				events: {
+					'onReady': onPlayerReady
+				}
+			});
+		});
+	};
+
+	window.onPlayerReady = function(event) {
+		$('.start-' + event.target.h.id).on('click', function() {
+			event.target.playVideo();
+		})
+	};
+
 	$(document).ready(function() {
 		$video = $('.exit-video').find('video');
 		$video.on('hover', play_video);
 		$video.on('click', play_video);
 
-		// 2. This code loads the IFrame Player API code asynchronously.
 		var tag = document.createElement('script');
 
 		tag.src = "https://www.youtube.com/iframe_api";
 		var firstScriptTag = document.getElementsByTagName('script')[0];
 		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-		var players = [];
-
-		window.onYouTubeIframeAPIReady = function() {
-			$('.inline-youtube-video').each(function(){
-				var video_id = $(this).data('video-id'),
-					video_height = $(this).data('video-height'),
-					video_width = $(this).data('video-width');
-
-				players.push(video_id);
-				players[video_id] = new YT.Player( 'youtube-video-' + video_id, {
-					height: video_height,
-					width: video_width,
-					videoId: video_id,
-					playerVars: {
-						modestbranding: 1,
-						showinfo: 0,
-						controls: 0,
-						rel: 0
-					},
-					events: {
-						'onReady': onPlayerReady
-					}
-				});
-			});
-		};
-
-		window.onPlayerReady = function(event) {
-			$('.start-' + event.target.h.id).on('click', function() {
-				event.target.playVideo();
-			})
-		};
 	});
 
 })(jQuery, window);
