4e50255913de876fbfbb3a137a9f8ac07f914880
jeremyfelt <jeremy.felt@gmail.com>
2015-03-23 22:53:45 -0700

    Bump version 0.7.3

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ class WSU_Home_Theme {
 	/**
 	 * @var string The version of the WSU Home theme for cache breaking.
 	 */
-	var $version = '0.7.2';
+	var $version = '0.7.3';
 
 	/**
 	 * Configure our default hooks.
