37c776f219cc429f300ce31c8abb4cbe90f25247
Jeremy Bass <jeremybass@cableone.net>
2015-05-18 10:12:09 -0700

    bump version to 0.8.4

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ class WSU_Home_Theme {
 	/**
 	 * @var string The version of the WSU Home theme for cache breaking.
 	 */
-	var $version = '0.8.3';
+	var $version = '0.8.4';
 
 	/**
 	 * Configure our default hooks.
