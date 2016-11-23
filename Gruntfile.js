3396bc26408d6607ddeaf95e313edf45bbccd9e7
Jeremy Felt <jeremy.felt@gmail.com>
2016-11-23 11:07:12 -0800

    Add WSU theme skeleton structure
    In this first iteration, we're ignoring CSS linting, but starting
    to handle the JavaScript in its new location.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,79 +1,190 @@
-module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+var Promise = require( "es6-promise" ).polyfill();
+
+module.exports = function( grunt ) {
+	grunt.initConfig( {
+		pkg: grunt.file.readJSON( "package.json" ),
+
+		stylelint: {
+			src: [ "css/*.css" ]
+		},
 
 		concat: {
-			media_wall: {
-				src: [
-					'includes/js/media-wall-item-model.js',
-					'includes/js/media-wall-item-view.js',
-					'includes/js/media-wall-view.js',
-					'includes/js/media-wall.js'
-				],
-				dest: 'includes/js/media-wall.full.js'
-			},
 			home_scripts: {
 				src: [
-					'js/wsu-home-fos-view.js',
-					'js/wsu-home-navigation-view.js',
-					'js/wsu-home-primary.js'
+					"src/js/wsu-home-fos-view.js",
+					"src/js/wsu-home-navigation-view.js",
+					"src/js/wsu-home-primary.js"
 				],
-				dest: 'js/wsu-home.js'
+				dest: "js/wsu-home.js"
 			},
 			feature_scripts: {
 				src: [
-					'js/wsu-home-navigation-view.js',
-					'js/wsu-home-primary.js',
-					'js/wsu-feature-primary.js'
+					"src/js/wsu-home-navigation-view.js",
+					"src/js/wsu-home-primary.js",
+					"src/js/wsu-feature-primary.js"
 				],
-				dest: 'js/wsu-feature.js'
+				dest: "js/wsu-feature.js"
 			}
 		},
 
-		uglify: {
-			media_wall: {
-				src: 'includes/js/media-wall.full.js',
-				dest: 'includes/js/media-wall.min.js'
+		postcss: {
+			options: {
+				map: true,
+				diff: false,
+				processors: [
+					require( "autoprefixer" )( {
+						browsers: [ "> 1%", "ie 8-11", "Firefox ESR" ]
+					} )
+				]
 			},
-			home_scripts: {
-				src: 'js/wsu-home.js',
-				dest: 'js/wsu-home.min.js'
-			},
-			feature_scripts: {
-				src: 'js/wsu-feature.js',
-				dest: 'js/wsu-feature.min.js'
+			dist: {
+				src: "tmp-style.css",
+				dest: "style.css"
+			}
+		},
+
+		csslint: {
+			main: {
+				src: [ "style.css" ],
+				options: {
+					"fallback-colors": false,              // Unless we want to support IE8
+					"box-sizing": false,                   // Unless we want to support IE7
+					"compatible-vendor-prefixes": false,   // The library on this is older than autoprefixer.
+					"gradients": false,                    // This also applies ^
+					"overqualified-elements": false,       // We have weird uses that will always generate warnings.
+					"ids": false,
+					"regex-selectors": false,
+					"adjoining-classes": false,
+					"box-model": false,
+					"universal-selector": false,
+					"unique-headings": false,
+					"outline-none": false,
+					"floats": false,
+					"font-sizes": false,
+					"important": false,                    // This should be set to 2 one day.
+					"unqualified-attributes": false,       // Should probably be 2 one day.
+					"qualified-headings": false,
+					"known-properties": 1,                 // Okay to ignore in the case of known unknowns.
+					"duplicate-background-images": 2,
+					"duplicate-properties": 2,
+					"star-property-hack": 2,
+					"text-indent": 2,
+					"display-property-grouping": 2,
+					"shorthand": 2,
+					"empty-rules": false,
+					"vendor-prefix": 2,
+					"zero-units": 2,
+					"order-alphabetical": false
+				}
 			}
 		},
 
 		clean: {
-			temporary: {
-				src: [
-					'includes/js/media-wall.full.js',
-					'js/wsu-home.js',
-					'js/wsu-feature.js'
-				]
+			options: {
+				force: true
+			},
+			temp: [ "tmp-style.css", "tmp-style.css.map" ]
+		},
+
+		jscs: {
+			scripts: {
+				src: [ "Gruntfile.js", "src/js/*.js" ],
+				options: {
+					preset: "jquery",
+					requireCamelCaseOrUpperCaseIdentifiers: false, // We rely on name_name too much to change them all.
+					maximumLineLength: 250
+				}
+			}
+		},
+
+		jshint: {
+			grunt_script: {
+				src: [ "Gruntfile.js" ],
+				options: {
+					curly: true,
+					eqeqeq: true,
+					noarg: true,
+					quotmark: "double",
+					undef: true,
+					unused: false,
+					node: true     // Define globals available when running in Node.
+				}
+			},
+			theme_scripts: {
+				src: [ "src/js/*.js" ],
+				options: {
+					bitwise: true,
+					curly: true,
+					eqeqeq: true,
+					forin: true,
+					freeze: true,
+					noarg: true,
+					nonbsp: true,
+					quotmark: "double",
+					undef: true,
+					unused: true,
+					browser: true, // Define globals exposed by modern browsers.
+					jquery: true   // Define globals exposed by jQuery.
+				}
+			}
+		},
+
+		uglify: {
+			home_scripts: {
+				src: "js/wsu-home.js",
+				dest: "js/wsu-home.min.js"
+			},
+			feature_scripts: {
+				src: "js/wsu-feature.js",
+				dest: "js/wsu-feature.min.js"
+			}
+		},
+
+		phpcs: {
+			plugin: {
+				src: "./"
+			},
+			options: {
+				bin: "vendor/bin/phpcs --extensions=php --ignore=\"*/vendor/*,*/node_modules/*\"",
+				standard: "phpcs.ruleset.xml"
 			}
 		},
 
 		watch: {
-			files: [
-				'includes/js/media-wall-item-model.js',
-				'includes/js/media-wall-item-view.js',
-				'includes/js/media-wall-view.js',
-				'includes/js/media-wall.js',
-				'js/wsu-home-fos-view.js',
-				'js/wsu-home-navigation-view.js',
-				'js/wsu-home-primary.js'
-			],
-			tasks: ['default']
-		}
-	});
+			styles: {
+				files: [ "css/*.css", "src/js/*.js" ],
+				tasks: [ "default" ],
+				option: {
+					livereload: 8000
+				}
+			}
+		},
 
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+		connect: {
+			server: {
+				options: {
+					open: true,
+					port: 8000,
+					hostname: "localhost"
+				}
+			}
+		}
+
+	} );
+
+	grunt.loadNpmTasks( "grunt-postcss" );
+	grunt.loadNpmTasks( "grunt-contrib-concat" );
+	grunt.loadNpmTasks( "grunt-contrib-csslint" );
+	grunt.loadNpmTasks( "grunt-contrib-clean" );
+	grunt.loadNpmTasks( "grunt-phpcs" );
+	grunt.loadNpmTasks( "grunt-contrib-watch" );
+	grunt.loadNpmTasks( "grunt-contrib-connect" );
+	grunt.loadNpmTasks( "grunt-jscs" );
+	grunt.loadNpmTasks( "grunt-contrib-jshint" );
+	grunt.loadNpmTasks( "grunt-contrib-uglify" );
+	grunt.loadNpmTasks( "grunt-stylelint" );
 
 	// Default task(s).
-	grunt.registerTask('default', ['concat', 'uglify', 'clean']);
+	grunt.registerTask( "default", [ "jscs", "jshint", "concat", "uglify", "clean" ] );
+	grunt.registerTask( "serve", [ "connect", "watch" ] );
 };
