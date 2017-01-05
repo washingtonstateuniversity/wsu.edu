bf29c3e245700c146987d111841b99d0f4df83a5
Jeremy Felt <jeremy.felt@gmail.com>
2017-01-04 18:07:19 -0800

    Use tabs instead of spaces
    My IDE hates me

--- 404.php
+++ 404.php
@@ -6,30 +6,30 @@
 get_header();
 ?>
 	<main id="wsuwp-main" class="spine-blank-template wsu-error-content">
-        <section class="row side-right gutter">
+		<section class="row side-right gutter">
 
-            <div class="column one">
+			<div class="column one">
 
-                <h2>Page not found. <span class="error-404-text">404 Error</span></h2>
+				<h2>Page not found. <span class="error-404-text">404 Error</span></h2>
 
-                <p>
-                    The address that you tried was not found. You may have used an outdated link or typed the URL incorrectly. Please try our site search or one of these links.
-                </p>
+				<p>
+					The address that you tried was not found. You may have used an outdated link or typed the URL incorrectly. Please try our site search or one of these links.
+				</p>
 
-                <p>
-                    <ul>
-                        <li><a href="https://index.wsu.edu">The A-Z Directory</a> (an alphabetical list of departments, programs, resources, organizations, and more)</li>
-                        <li><a href="https://wsu.edu/academics/">Academic Departments</a> (a list of links to all academic departments)</li>
-                        <li><a href="https://wsu.edu/about/services/">Administrative Offices and Resources</a> (a list of links to all administrative offices and resources)</li>
-                        <li><a href="https://wsu.edu/admission/">Admissions and Aid</a> (information for prospective students about applying, tuition, financial aid, and more)</li>
-                        <li><a href="https://wsu.edu/contact/">Contact Us</a> (phone and email contacts)</li>
-                    </ul>
-                </p>
-            </div><!--/column-->
+				<p>
+					<ul>
+						<li><a href="https://index.wsu.edu">The A-Z Directory</a> (an alphabetical list of departments, programs, resources, organizations, and more)</li>
+						<li><a href="https://wsu.edu/academics/">Academic Departments</a> (a list of links to all academic departments)</li>
+						<li><a href="https://wsu.edu/about/services/">Administrative Offices and Resources</a> (a list of links to all administrative offices and resources)</li>
+						<li><a href="https://wsu.edu/admission/">Admissions and Aid</a> (information for prospective students about applying, tuition, financial aid, and more)</li>
+						<li><a href="https://wsu.edu/contact/">Contact Us</a> (phone and email contacts)</li>
+					</ul>
+				</p>
+			</div><!--/column-->
 
-            <div class="column two"></div>
+			<div class="column two"></div>
 
-        </section>
+		</section>
 	</main>
 <?php
 
