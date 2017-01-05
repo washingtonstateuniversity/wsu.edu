acedb2b8054670e4d22ceefa8feac77691660e2a
Jeremy Felt <jeremy.felt@gmail.com>
2017-01-04 17:29:29 -0800

    Style the text on the 404 display to be closer to mock-up
    This may still need some changes, but should do as a first version

--- 404.php
+++ 404.php
@@ -5,12 +5,12 @@
 
 get_header();
 ?>
-	<main id="wsuwp-main" class="spine-blank-template">
-        <section class="row side-right gutter pad-ends">
+	<main id="wsuwp-main" class="spine-blank-template wsu-error-content">
+        <section class="row side-right gutter">
 
             <div class="column one">
 
-                <h1>Page not found. <span class="404-error">404 Error</span></h1>
+                <h2>Page not found. <span class="error-404-text">404 Error</span></h2>
 
                 <p>
                     The address that you tried was not found. You may have used an outdated link or typed the URL incorrectly. Please try our site search or one of these links.
