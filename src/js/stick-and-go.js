2219ea95b61bb2b1dde39be6acc63e9b1730dc2d
Phil Cable <pcable@wsu.edu>
2017-05-03 15:20:21 -0700

    Refactor Stick and Go library

--- src/js/stick-and-go.js
+++ src/js/stick-and-go.js
@@ -1,44 +1,26 @@
 ( function( $, window ) {
 
-	// Determine and set image widths so they don't collapse when positioned.
-	function image_width() {
-		$( ".card-image" ).each( function() {
-			var $image = $( this ),
-				$container = $image.closest( ".column" );
-
-			if ( 989 < $( window ).width() ) {
-				$image.width( $container.width() );
-			} else {
-				$image.removeClass( "fixed absolute" );
-			}
-		} );
-	}
-
 	// Fix images in place when they reach the top of the viewport,
 	// scroll as normal when the bottom of the container is in the viewport.
 	function stick_and_go() {
-		$( ".card" ).each( function() {
-			var card = this.getBoundingClientRect(),
+		$( ".ui-stick-and-go > *" ).each( function() {
+			var element = this.getBoundingClientRect(),
 				window_height = $( window ).height(),
-				$image = $( this ).find( ".card-image" );
+				$image = $( this ).find( "figure" );
 
-			if ( 0 >= card.top && card.bottom >= window_height ) {
+			if ( 0 >= element.top && element.bottom >= window_height ) {
 				$image.addClass( "fixed" );
 			} else {
 				$image.removeClass( "fixed" );
 			}
 
-			if ( card.bottom <= window_height ) {
+			if ( element.bottom <= window_height ) {
 				$image.addClass( "absolute" );
 			} else {
 				$image.removeClass( "absolute" );
 			}
 		} );
 	}
-
-	$( window ).resize( function() {
-		image_width();
-	} );
 
 	$( window ).scroll( function() {
 		if ( 989 < $( window ).width() ) {
---
+++ src/js/top-ten.js
@@ -0,0 +1,27 @@
+( function( $, window ) {
+
+	// Removes the `src` from each image. (Madness!)
+	// Loads the appropriate image for the screen size.
+	// Determines and sets image widths so they don't rescale when positioned.
+	function process_card_images() {
+		$( ".content-card--feature-image" ).each( function() {
+			var $figure = $( this ),
+				$image = $figure.find( "img" ),
+				$card = $figure.closest( ".content-card" ),
+				desktop_image = $figure.data( "desktop-image" ),
+				mobile_image = $figure.data( "mobile-image" );
+
+			$image.removeAttr( "src" );
+
+			if ( 989 < $( window ).width() && desktop_image ) {
+				$image.css( "background-image", "url(" + desktop_image + ")" ).width( $card.width() / 2 );
+			} else if ( mobile_image ) {
+				$image.css( "background-image", "url(" + mobile_image + ")" ).removeClass( "fixed absolute" );
+			}
+		} );
+	}
+
+	$( window ).resize( function() {
+		process_card_images();
+	} );
+}( jQuery, window ) );
