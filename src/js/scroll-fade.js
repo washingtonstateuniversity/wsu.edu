cd8a48a715d9aa350a52bf89e751f6183c7efc35
Phil Cable <pcable@wsu.edu>
2017-05-04 09:46:48 -0700

    Refactor scroll fade library
    This now acts directly on elements with the `ui-scroll-fade` class

--- src/js/scroll-fade.js
+++ src/js/scroll-fade.js
@@ -1,34 +1,33 @@
 ( function( $, window ) {
 
-	// Load the appropriate image for the screen size.
-	function reset_opacity() {
-		$( ".ui-scroll-fade .content-card--feature-image" ).each( function() {
-			if ( 989 < $( window ).width() ) {
-				$( this ).css( "opacity", 1 );
-			}
-		} );
-	}
-
-	// Reduce the opacity of images as they are scrolled up out of the viewport.
+	// Reduce the opacity of elements as they are scrolled up out of the viewport.
 	function scroll_fade() {
-		$( ".ui-scroll-fade > *" ).each( function() {
-			var element = this.getBoundingClientRect(),
-				window_height = $( window ).height(),
-				$image = $( this ).find( ".content-card--feature-image" );
+		$( ".ui-scroll-fade" ).each( function() {
+			var $element = $( this ),
+				element = this.getBoundingClientRect(),
+				container = $element.parent()[ 0 ].getBoundingClientRect(),
+				window_height = $( window ).height();
 
-			if ( 0 <= element.bottom ) {
-				$image.css( "opacity", element.bottom / window_height );
+			if ( 0 < container.bottom && 0 > container.top ) {
+				$element.css( "opacity", element.bottom / window_height );
 			}
 		} );
 	}
 
-	$( window ).resize( function() {
-		reset_opacity();
-	} );
+	// Reset element opacity.
+	function reset_opacity() {
+		$( ".ui-scroll-fade" ).css( "opacity", 1 );
+	}
 
 	$( window ).scroll( function() {
 		if ( 989 < $( window ).width() ) {
 			window.requestAnimationFrame( scroll_fade );
+		}
+	} );
+
+	$( window ).resize( function() {
+		if ( 989 > $( window ).width() ) {
+			reset_opacity();
 		}
 	} );
 }( jQuery, window ) );
