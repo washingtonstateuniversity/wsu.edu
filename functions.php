8b81f9ff515b10f589d612d32cb76336b46b8cca
Phil Cable <pcable@wsu.edu>
2017-05-04 13:43:33 -0700

    Introduce a shortcode for displaying content cards
    Fairly specific to the Top Ten page

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ include_once( __DIR__ . '/includes/fields-of-study.php' );
 include_once( __DIR__ . '/includes/academic-calendar.php' );
 include_once( __DIR__ . '/includes/wsuwp-map-embed.php' );
 include_once( __DIR__ . '/includes/feature-youtube-embed.php' );
+include_once( __DIR__ . '/includes/top-ten-card-shortcode.php' );
 
 class WSU_Home_Theme {
 	/**
