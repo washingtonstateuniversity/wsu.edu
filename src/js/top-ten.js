cb2d0a377db93a8f18332b73537fd9c37c260169
Phil Cable <pcable@wsu.edu>
2017-05-05 15:07:29 -0700

    Remove obsolete image processing JavaScript
    This is no longer being used as of https://github.com/washingtonstateuniversity/wsu.edu/commit/1eabae96bbba25824e552b68a6368a152fb3b1af

--- src/js/top-ten.js
+++ src/js/top-ten.js
@@ -1,6 +1,5 @@
 ( function( $, window ) {
 
-	// Removes the `src` from each image. (Madness!)
 	// Loads the appropriate image for the screen size.
 	// Determines and sets image widths so they don't rescale when positioned.
 	function process_card_images() {
@@ -10,8 +9,6 @@
 				$card = $figure.closest( ".content-card" ),
 				desktop_image = $figure.data( "desktop-image" ),
 				mobile_image = $figure.data( "mobile-image" );
-
-			$image.removeAttr( "src" );
 
 			if ( 989 < $( window ).width() && desktop_image ) {
 				$image.css( {
