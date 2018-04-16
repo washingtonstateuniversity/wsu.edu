f24026a5642729e2c7ec90a5b665d09260e6ec75
Jeremy Felt <jeremy.felt@gmail.com>
2018-04-16 14:33:51 -0700

    Remove featured image HTML from home page

--- templates/home-page.php
+++ templates/home-page.php
@@ -13,7 +13,6 @@ get_header();
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 		<?php get_template_part( 'parts/headers' ); ?>
-		<?php get_template_part( 'parts/featured-images' ); ?>
 
 		<div id="page-<?php the_ID(); ?>" <?php post_class(); ?>>
 			<?php the_content(); ?>
