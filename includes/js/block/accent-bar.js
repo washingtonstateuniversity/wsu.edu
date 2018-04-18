49c0bf5d6745eb21b90f237166a0045107ad8094
Jeremy Felt <jeremy.felt@gmail.com>
2018-04-18 15:51:15 -0700

    Adjust capture and display of accent bar

--- includes/js/block/accent-bar.js
+++ includes/js/block/accent-bar.js
@@ -1,34 +1,90 @@
 const {
 	Fragment,
+	RawHTML,
 } = wp.element;
 
 const {
 	registerBlockType,
+	PlainText,
+	InspectorControls,
 } = wp.blocks;
+
+const {
+	PanelBody,
+} = wp.components;
 
 registerBlockType( 'wsu/accent-bar', {
 	title: 'Accent bar',
 
-	description: 'Display a crimson accent bar',
+	description: 'Display a crimson accent bar with optional call to action or announcement text',
 
 	category: 'layout',
 
 	icon: 'admin-appearance',
 
-	edit() {
-		return (
-			<Fragment key="edit">
-				<section className="row single gutter pad-top accent-bar">
-					<div className="column one"></div>
-				</section>
-			</Fragment>
-		);
+	attributes: {
+		accent_bar_text: {
+			type: 'string',
+		},
+		accent_bar_url: {
+			type: 'string',
+		},
 	},
 
-	save() {
+	edit( { attributes, setAttributes } ) {
+		const {
+			accent_bar_text,
+			accent_bar_url,
+		} = attributes;
+
+		return [
+			<InspectorControls key="inspector">
+				<PanelBody title="Accent bar information">
+					<PlainText
+						placeholder=""
+						value={ accent_bar_text }
+						onChange={ ( accent_bar_text ) => setAttributes( { accent_bar_text } ) }
+					/>
+					<PlainText
+						placeholder="Accent bar link"
+						value={ accent_bar_url }
+						onChange={ ( accent_bar_url ) => setAttributes( { accent_bar_url } ) }
+					/>
+				</PanelBody>
+			</InspectorControls>,
+			<Fragment key="edit">
+				<section className="row single gutter pad-top accent-bar">
+					<div className="column one">
+						<div>
+							<span>{ accent_bar_text }</span>
+						</div>
+					</div>
+				</section>
+			</Fragment>
+		];
+	},
+
+	save( { attributes } ) {
+		const {
+			accent_bar_text,
+			accent_bar_url,
+		}  = attributes;
+
+		let final_html = '';
+
+		if ( accent_bar_url && accent_bar_text && '' !== accent_bar_text ) {
+			final_html = `<a href="${ accent_bar_url }">${ accent_bar_text }</a>`
+		} else {
+			final_html = accent_bar_text;
+		}
+
 		return (
 			<section className="row single gutter pad-top accent-bar">
-				<div className="column one"></div>
+				<div className="column one">
+					<div>
+						<RawHTML>{ final_html }</RawHTML>
+					</div>
+				</div>
 			</section>
 		);
 	},
