a48c88ac875c878b9f43ccde23c2773e67c8fa7e
Jeremy Felt <jeremy.felt@gmail.com>
2018-04-30 12:31:29 -0700

    Add a comment explaning button states across nav

--- src/block/site-header-mega/index.js
+++ src/block/site-header-mega/index.js
@@ -4,6 +4,8 @@ navigation_buttons.forEach( function( el ) {
 	el.addEventListener( "click", function() {
 		let is_aria_expanded = this.getAttribute( "aria-expanded" ) === 'true' || false;
 
+		// Each button in the main navigation has a mirrored state, so we adjust
+		// aria-expanded and hidden on all sub-navigation on every button interaction.
 		navigation_buttons.forEach( function( el ) {
 			el.setAttribute( "aria-expanded", ! is_aria_expanded );
 
