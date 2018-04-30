a483fd921cd00df1dfb37e7c2b04a250bf5e8b59
Jeremy Felt <jeremy.felt@gmail.com>
2018-04-30 14:40:09 -0700

    Close menu when Escape button is used

--- src/block/navigation/index.js
+++ src/block/navigation/index.js
@@ -1,16 +1,38 @@
-const navigation_buttons = document.querySelectorAll(".main-navigation .nav-dropdown button");
+const navigation = document.querySelector( ".main-navigation .nav-dropdown" );
+const navigation_buttons = navigation.querySelectorAll( "button" );
+const navigation_sub_menus = navigation.querySelectorAll( ".sub-navigation" );
+const escape_key = 27;
 
-navigation_buttons.forEach(function (el) {
-	el.addEventListener("click", function () {
-		let is_aria_expanded = this.getAttribute("aria-expanded") === 'true' || false;
+navigation.addEventListener( "keydown", function ( event ) {
+
+	// When Escape is used in the navigation, close the menu.
+	if ( event.keyCode === 27 ) {
+		navigation_buttons.forEach( function ( el ) {
+			el.setAttribute( "aria-expanded", false );
+			el.nextElementSibling.hidden = true;
+		} );
+
+		// If the focus was on a child element of the nav section, reapply
+		// focus to the parent section's button.
+		event.path.forEach( function( el ) {
+			if ( el.localName === "li" && el.classList.contains( "nav-section" ) ) {
+				el.querySelector( "button" ).focus();
+			}
+		} );
+	}
+} );
+
+navigation_buttons.forEach( function ( el ) {
+	el.addEventListener( "click", function () {
+		let is_aria_expanded = this.getAttribute( "aria-expanded" ) === 'true' || false;
 
 		// Each button in the main navigation has a mirrored state, so we adjust
 		// aria-expanded and hidden on all sub-navigation on every button interaction.
-		navigation_buttons.forEach(function (el) {
-			el.setAttribute("aria-expanded", !is_aria_expanded);
+		navigation_buttons.forEach( function ( el ) {
+			el.setAttribute( "aria-expanded", ! is_aria_expanded );
 
 			let el_menu = el.nextElementSibling;
 			el_menu.hidden = !el_menu.hidden;
-		});
-	});
-});
+		} );
+	} );
+} );
